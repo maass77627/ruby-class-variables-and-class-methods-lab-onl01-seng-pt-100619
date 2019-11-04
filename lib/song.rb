5a76a4292fe91a731ca4e71c2d04cadd7980aa86
kelly r maass <kmaass77627@gmail.com>
2019-11-04 03:22:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,9 +36,9 @@ class Song
      genres_to_return = {}
       @@genres.each do |genre|
       if @@genre_count.has_key?(genre)
-        genres_to_return[genre] += 1
+        genres_to_return[genre]+= 1
       else
-        genres_to_return[genre] = 1      
+        genres_to_return[genre]= 1      
     end
     end
     genres_to_return 
