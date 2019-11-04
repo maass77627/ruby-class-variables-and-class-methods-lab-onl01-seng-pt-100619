786513394ec724e1a5f2a9e2e40fca263b2dc17c
kelly r maass <kmaass77627@gmail.com>
2019-11-04 02:22:28 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+'require pry'
+
+class Song 
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@count = 0 
+  
+  @@genres = []
+  
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist 
+    @genre = genre 
+    @@count += 1 
+    @@genres << genre 
+    @@artists << artist 
+  end 
+  
+  def self.count
+    @@count 
+  end 
+  
+  def self.genres 
+    @@genres 
+  end
+  
+  def self.artists(artist) 
+   artists.include?(artist)
+   artists << artist
+end 
+  
+end 
+    
