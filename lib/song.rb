25983aed4082e12fbe92d868e77b79a3daf007ad
kelly r maass <kmaass77627@gmail.com>
2019-11-04 02:58:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,8 +34,8 @@ class Song
   
   def self.genre_count 
     @@genres.each do |genre|
-      if genre_count.has_key?(#{genre})
-        genre_count[genre] += 1
+      if @@genre_count.has_key?(:genre)
+        @@genre_count[genre] += 1
       else
         genre_count[genre] = 1
       end
