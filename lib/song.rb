5f15e5d11f3df1bc60d3e11db935ff803290e09d
kelly r maass <kmaass77627@gmail.com>
2019-11-04 02:58:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,7 +37,7 @@ class Song
       if @@genre_count.has_key?(:genre)
         @@genre_count[genre] += 1
       else
-        genre_count[genre] = 1
+        @@genre_count[genre] = 1
       end
     end
   end
